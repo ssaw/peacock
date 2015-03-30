3a8f00e6b2db5c05516be2107a9fc62c8139b04a
Raimon Lapuente <r.lapuente@gmail.com>
2015-03-30 23:42:00 +0100

    Adding text file and font test

---
+++ PeacockTests/LYPeacockTextFieldTests.m
@@ -0,0 +1,50 @@
+//
+//  Created by Raimon Lapuente on 30/3/15.
+//  Copyright (c) 2015 Lyst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import <KIF/KIF.h>
+
+#import "UITextField+LYAppearance.h"
+
+NSString *helveticaUltraLight = @"HelveticaNeue-UltraLight";
+
+@interface LYPeacockTextFieldTests : KIFTestCase
+
+@property (nonatomic, strong, readonly) UIViewController *SUTController;
+@property (nonatomic, strong, readonly) UIWindow *window;
+@property (nonatomic ,strong, readonly) UITextField *SUTTextField;
+
+@end
+
+@implementation LYPeacockTextFieldTests
+
+- (void)setUp {
+    [super setUp];
+    
+    _SUTController = [[UIViewController alloc] init];
+    _SUTTextField = [[UITextField alloc] init];
+    [_SUTController.view addSubview:_SUTTextField];
+    
+    _window = [[UIWindow alloc]initWithFrame:CGRectMake(0.0, 0.0, 320.0, 468.0)];
+    [_window setRootViewController:self.SUTController];
+}
+
+-(void)testThatWeCanSetTextFieldFont
+{
+    //given
+    [[UITextField appearance] setLYAppearanceFont:[UIFont fontWithName:helveticaUltraLight size:20.0f]];
+    
+    //when
+    [self.window makeKeyAndVisible];
+    
+    //then
+    UIFont *textFont = self.SUTTextField.font;
+    XCTAssertTrue([helveticaUltraLight isEqualToString:textFont.fontName]);
+    
+}
+
+
+@end
