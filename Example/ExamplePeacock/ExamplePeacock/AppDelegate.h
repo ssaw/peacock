0bc143560e252332b1f0a8409ea96ebc577a6c93
Raimon Lapuente <r.lapuente@gmail.com>
2015-04-20 22:58:26 +0100

    Example project for Peacock

---
+++ Example/ExamplePeacock/ExamplePeacock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExamplePeacock
+//
+//  Created by Raimon Lapuente on 20/04/2015.
+//  Copyright (c) 2015 Lyst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/ExamplePeacock/ExamplePeacock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExamplePeacock
+//
+//  Created by Raimon Lapuente on 20/04/2015.
+//  Copyright (c) 2015 Lyst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
